345867659c833ff44adbd4e97053bf6b8e2d9ac8
Technici4n <bruno.ploumhans@gmail.com>
2017-06-13 21:36:48 +0200

    UVa submissions and problems

--- handler.js
+++ handler.js
@@ -2,6 +2,8 @@
 const scores = require("./lib/scores")
 const activities = require("./lib/activities")
 const status = require("./lib/status")
+const submissions = require("./lib/submissions");
+const problems = require("./lib/problems");
 
 function db_client() {
   let pg = require('pg');
@@ -50,4 +52,26 @@ completed for the first time since the last time it was checked.
 */
 module.exports.activities_compute = (event, context, callback) => {
   return activities.compute(db_client(), callback);
+};
+
+/**
+ * Gather all the submissions of a given user
+ */
+module.exports.submissions_user = (event, context, callback) => {
+  return submissions.user(db_client(), event, callback);
+};
+
+/**
+ * Gather all the problems
+ */
+module.exports.problems_all = (event, context, callback) => {
+  return problems.all(db_client(), callback);
+};
+
+/**
+ * Update UVa problems
+ */
+module.exports.uva_update_problems = (event, context, callback) => {
+  require('./lib/platforms/uva.js').update_problems(db_client());
+  callback();
 };
